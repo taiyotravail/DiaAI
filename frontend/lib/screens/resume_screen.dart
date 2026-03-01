7ae21f1ccc3eca9c651853720eb5faa26c1c8aba
DEGARATE Philippe <taiyo.travail@gmail.com>
2026-03-01 13:39:41 +0100

    Affichage de la dernière prédiction sur la page résumé

--- frontend/lib/screens/resume_screen.dart
+++ frontend/lib/screens/resume_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../main.dart';
+import '../widgets/prediction_card.dart';
 
 class ResumeScreen extends StatelessWidget {
   const ResumeScreen({super.key});
@@ -13,8 +15,38 @@ class ResumeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: const Center(
-        child: Text('Résumé de l\'application'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              "Dernière prédiction",
+              style: TextStyle(color: Colors.grey[600], fontSize: 12),
+            ),
+            FutureBuilder(
+              future: predictionRepository.getAllPredictions(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text("Chargement...");
+                }
+
+                if (snapshot.hasError) {
+                  return Text("Erreur: ${snapshot.error}");
+                }
+
+                final predictions = snapshot.data ?? [];
+                if (predictions.isEmpty) {
+                  return const Text("Aucune prédiction");
+                }
+                else {
+                  return PredictionCard(
+                    prediction: predictions.last
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
